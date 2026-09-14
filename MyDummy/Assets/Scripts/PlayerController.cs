cea650c901a0d89343bd5c8534a3f02994136202
dotcom4143 <eostmfhektzja4143@gmail.com>
2026-09-14 12:04:25 +0900

    [Feat] VoxEdit 임포트 & 가만히 있기, 걷기, 뛰기 완성

---
+++ MyDummy/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,99 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private Transform cameraTransform;
+
+    [Header("이동 설정")]
+    [SerializeField] private float walkSpeed = 3f;
+    [SerializeField] private float runSpeed = 6f;
+    [SerializeField] private float rotationSpeed = 10f;
+
+    [Header("바닥 설정")]
+    [SerializeField] private float gravity = -20f;
+
+    private CharacterController controller;
+
+    private float verticalVelocity;
+
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        Keyboard keyboard = Keyboard.current;
+
+        if (keyboard == null)
+        {
+            return;
+        }
+        //1. WASD 입력
+        Vector2 input = Vector2.zero;
+
+        if (keyboard.aKey.isPressed)
+            input.x -= 1f;
+        if (keyboard.dKey.isPressed)
+            input.x += 1f;
+        if (keyboard.sKey.isPressed)
+            input.y -= 1f;
+        if (keyboard.wKey.isPressed)
+            input.y += 1f;
+
+        input = Vector2.ClampMagnitude(input, 1f);
+        
+        //2. 카메라의 앞쪽과 오른쪽 방향
+        Vector3 cameraForward = cameraTransform.forward;
+        Vector3 cameraRight = cameraTransform.right;
+
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        //3. 카메라 기준 이동 방향
+        Vector3 moveDirection = cameraForward * input.y + cameraRight * input.x;
+        moveDirection = Vector3.ClampMagnitude(moveDirection, 1f);
+
+        //4. Shift 키로 달리기
+        bool isRunning = keyboard.leftShiftKey.isPressed;
+        float currentSpeed = isRunning ? runSpeed : walkSpeed;
+
+        //5. 수평 이동
+        controller.Move(moveDirection * currentSpeed * Time.deltaTime);
+        
+        //6. 이동 방향으로 회전
+        if (moveDirection.sqrMagnitude > 0.001f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        //7. 기본 중력 설정
+        if (controller.isGrounded && verticalVelocity < 0f)
+        {
+            verticalVelocity = -2f;
+        }
+        else
+        {
+            verticalVelocity += gravity * Time.deltaTime;
+        }
+
+        controller.Move(Vector3.up * verticalVelocity * Time.deltaTime);
+
+        //8.Idle, Walk, Run 애니메이션
+        float animationSpeed = 0f;
+
+        if (moveDirection.sqrMagnitude > 0.001f)
+        {
+            animationSpeed = isRunning ? 1f : 0.5f;
+        }
+
+        animator.SetFloat("speed", animationSpeed, 0.1f, Time.deltaTime);
+
+    }
+}
